958706225e4cdc273fe8d3474c511580b6ac388b
Rafael Henrique <henrique171999@hotmail.com>
2026-05-09 16:49:10 +0100

    Created project, properties configurations, created database in sql for the User and relations

---
+++ src/main/java/io/Rafa_lol/full_Project/FullProjectApplication.java
@@ -0,0 +1,13 @@
+package io.Rafa_lol.full_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/Rafa_lol/full_Project/FullProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Rafa_lol.full_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
